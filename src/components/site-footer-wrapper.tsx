59548c5e2b60568a3b458f19485b1f1f09c32d0d
HE11032006 <eulogemn@gmail.com>
2026-09-15 07:14:33 +0100

    fix: hide footer on admin route

--- src/components/site-footer-wrapper.tsx
+++ src/components/site-footer-wrapper.tsx
@@ -1,6 +1,9 @@
 "use client";
+import { usePathname } from "next/navigation";
 import SiteFooter from "./site-footer";
 
 export default function SiteFooterWrapper() {
+  const pathname = usePathname();
+  if (pathname.startsWith("/admin")) return null;
   return <SiteFooter />;
 }
