f77d03f12cb35f4b3ba8c9107f917085178e0f1b
HE11032006 <eulogemn@gmail.com>
2026-09-17 06:19:04 +0100

    update: line around link in footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -102,7 +102,7 @@ export default function SiteFooter() {
         style={{
           maxWidth: 1200,
           margin: "0 auto",
-          padding: "70px 24px 50px",
+          padding: "70px 24px 18px",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
